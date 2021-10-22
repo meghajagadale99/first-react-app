b5d7613e50b137471b87f363cc1cde53c88effb6
Megha <meghajagadale99@gmail.com>
2021-10-22 09:43:35 +0530

    Added: Hello from Bridgelabz page with complete text

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import img from './BL-logo.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         <h1>
-         Hello from BridgeLabz
-         </h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  url = 'https://www.bridgelabz.com/'
+  constructor() {
+    super()
+    this.state = {
+    userName: ' ',
+    nameError: ' '
+    
+    }
+  }
+  onClick = ($event) => {
+    console.log("Save Button is Clicked !", $event);
+    window.open(this.url, "_blank");
+  }
+
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2}$');
+    this.setState({userName: event.target.value})
+    if (nameRegex.test(event.target.value)) {
+      this.setState({nameError: ''})
+    } else{
+      this.setState({nameError: 'Name is Incorrect' })
+    }
+    }
+
+  render() {
+    return (
+      <div>
+        <div>
+        <h1>Hello {this.state.userName} from Bridgelabz!!!</h1>
+        <img src={img} onClick={this.onClick}
+        alt="This Bridgelabz logo: a Bridge to Employee through lab works" />
+        </div>
+        <div>
+        <input onChange={this.onNameChange} />
+        <span className="error-output">{this.state.nameError}</span>
+        </div>
+      </div>
+      
+    );
+  }
 }
-
 export default App;
